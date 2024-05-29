833adec7a5673a09012b966ed9ac59d06f483ee1
PTRRR <pietro.alberti@bluewin.ch>
2024-05-29 12:45:22 +0200

    Add params

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,17 +1,34 @@
 import Jimp from "jimp";
 import path from "path";
 import { generateSdf } from "../index";
+import { clamp } from "../utils";
 
 test("Main", async () => {
   const image = await Jimp.read(
     path.join(__dirname, "../../assets/atom-small.png")
   );
 
+  image.resize(128, 128);
+
   const imageData = new Uint8Array(image.bitmap.data.buffer);
 
   const sdf = generateSdf(imageData, {
     width: image.getWidth(),
+    invert: false,
+    outMin: -1000,
+    outMax: 1000,
   });
+
+  for (let i = 0; i < sdf.length; i++) {
+    const distance = clamp(sdf[i], 0, 255);
+    imageData[i * 4] = distance;
+    imageData[i * 4 + 1] = distance;
+    imageData[i * 4 + 2] = distance;
+    imageData[i * 4 + 3] = 255;
+  }
+
+  image.bitmap.data = Buffer.from(imageData);
+  image.write("test.png");
 
   expect(sdf.length).toBe(imageData.length / 4);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,90 @@
-import { clamp, remap } from "./utils";
+import { remap } from "./utils";
 import { sdf } from "./lib";
+
+export type ImageDataInput = ArrayLike<number>;
+
+export type GetFilledParams = {
+  invert?: boolean;
+  threshold?: number;
+  alpha?: boolean;
+};
 
 export type GenerateSdfParams = {
   width: number;
+  normalize?: boolean;
+  outMin?: number;
+  outMax?: number;
 };
 
-export const getFilled = (imageData: ArrayLike<number>) => {
+export const getFilled = (
+  imageData: ImageDataInput,
+  params?: GetFilledParams
+) => {
   const filled: boolean[] = [];
 
   for (let i = 0; i < imageData.length; i += 4) {
     const r = imageData[i];
     const g = imageData[i + 1];
     const b = imageData[i + 2];
+    const a = imageData[i + 3];
 
-    const isFilled = (r + g + b) / 3 < 255 / 2;
-    filled.push(isFilled);
+    const isFilled = params?.alpha
+      ? a < (params?.threshold || 255 / 2)
+      : (r + g + b) / 3 < (params?.threshold || 255 / 2);
+
+    filled.push(params?.invert ? !isFilled : isFilled);
   }
 
   return filled;
 };
 
 export const getDistances = (
-  imageData: Uint8Array,
-  params: GenerateSdfParams
+  imageData: ImageDataInput,
+  params: GenerateSdfParams & GetFilledParams
 ) => {
-  const filled = getFilled(imageData);
+  const filled = getFilled(imageData, params);
   return sdf(filled, params.width);
 };
 
 export const generateSdf = (
-  imageData: Uint8Array,
-  params: GenerateSdfParams
+  imageData: ImageDataInput,
+  params: GenerateSdfParams & GetFilledParams
 ) => {
   const distances = getDistances(imageData, params);
 
-  let maxValue = 0;
-  let minValue = Infinity;
+  let inMaxValue = 0;
+  let inMinValue = Infinity;
+  const outMaxValue = params.outMax || 255;
+  const outMinValue = params.outMin || 0;
 
-  for (const distance of distances) {
-    if (distance > maxValue) maxValue = distance;
-    if (distance < minValue) minValue = distance;
+  const doNormalize =
+    params.normalize || typeof params.normalize === "undefined";
+
+  if (doNormalize) {
+    for (const distance of distances) {
+      if (distance > inMaxValue) inMaxValue = distance;
+      if (distance < inMinValue) inMinValue = distance;
+    }
   }
 
   const newImageData: number[] = [];
 
   for (let i = 0; i < distances.length; i++) {
-    const distance = remap(distances[i], minValue, maxValue, 0, 255);
-    const clampedDistance = clamp(distance, 0, 255);
-    newImageData.push(clampedDistance);
+    const rawDistance = distances[i];
+
+    if (doNormalize) {
+      const distance = remap(
+        rawDistance,
+        inMinValue,
+        inMaxValue,
+        outMinValue,
+        outMaxValue
+      );
+
+      newImageData.push(distance);
+    } else {
+      newImageData.push(rawDistance);
+    }
   }
 
   return new Float32Array(newImageData);
